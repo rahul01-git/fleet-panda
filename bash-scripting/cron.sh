cc399a4da6f935a49ce9b5f46f96e003f8d3449d
rahul01-git <rahul.neupane0090@gmail.com>
2024-07-17 15:45:09 +0545

    day 2 html & day 3 css

---
+++ bash-scripting/cron.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# to enable debugging
+set -x
+
+# to exit on error occured
+#set -e
+
+#if [ $? -ne 0 ]; then
+#	echo "Error occured"
+#fi
+
+0 0 * * * /home/zodthegod/Desktop/fleet-panda/bootcamp/bash-scripting/num.sh
--- bash-scripting/gender.sh
+++ bash-scripting/gender.sh
@@ -3,22 +3,22 @@
 echo "Please enter your name: "
 read name
 
-echo "Please select your gender"
 echo "1. Male"
 echo "2. Female"
 echo "3. Others"
+echo "Please select your gender: "
 read gender
 case $gender in 
 	1)
-		echo "$name,  You are a male ! "
+		echo -e  "\n$name,  You are a male ! "
 		;;
 	2)
-		echo "$name, You are a female ! "
+		echo -e "\n$name, You are a female ! "
 		;;
 	3)
-		echo "$name, You are LGBTQ+ ! "
+		echo -e "\n$name, You are LGBTQ+ ! "
 		;;
 	*)
-		echo "$name, You're gender is not identified !"
+		echo -e "\n$name, You're gender is not identified !"
 		;;
 esac
