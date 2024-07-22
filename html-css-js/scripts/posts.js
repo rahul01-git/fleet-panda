b9795d21d400f379a9f80cc1eebad1ca7ff1ef3b
rahul01-git <rahul.neupane0090@gmail.com>
2024-07-22 16:17:49 +0545

    images along with posts

--- html-css-js/scripts/posts.js
+++ html-css-js/scripts/posts.js
@@ -5,20 +5,30 @@ if (!loggedIn) {
 } else {
   async function fetchPosts() {
     const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const imageRes = await fetch('https://jsonplaceholder.typicode.com/photos')
     const data = await response.json();
+    const imgData = await imageRes.json()
+
     const postsDiv = document.querySelector(".posts");
-    data.forEach((data) => {
+    data.forEach((data, i) => {
       const divEl = document.createElement("div");
       const titleEl = document.createElement("h3");
       const bodyEl = document.createElement("p");
+      const imgContainerEl = document.createElement('div')
+      const imageEl = document.createElement('img')
 
       titleEl.innerText = data.title;
       bodyEl.innerText = data.body;
+      imageEl.src = imgData[i].url
 
       divEl.classList.add("post");
       titleEl.classList.add("title");
       bodyEl.classList.add("body");
+      imgContainerEl.classList.add('img-container')
 
+      imgContainerEl.appendChild(imageEl)
+
+      divEl.appendChild(imgContainerEl)
       divEl.appendChild(titleEl);
       divEl.appendChild(bodyEl);
 
